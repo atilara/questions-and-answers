f582c432eb39eaa2d2c3e2c76cfd802ef5f9481d
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-02-19 11:06:20 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Importação do protocolo http
+const http = require("http");
+
+// Criando servidor adicionando uma porta
+// Função que recebe uma requisição e gera uma resposta
+http.createServer(function(request, response) {
+  response.end("Bem-vindo ao servidor");
+}).listen(5500);
+
+console.log("Servidor rodando");
