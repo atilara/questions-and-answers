1dcc7df91da27dc864b0cb44c5f57011a1de9a17
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-02-20 10:43:15 -0300

    Modifica função para utilizar query params

--- src/index.js
+++ src/index.js
@@ -7,15 +7,9 @@ const app = express();
 // Rota do tipo GET e seu caminho, recebendo request e response como params
 app.get('/', (req, res) => {
   // Enviando resposta para a rota
-  res.send('Olá!');
-});
-
-// Testando a utilização de parâmetros
-app.get('/:name', (req, res) => {
-  // Utilizando req para pegar o valor do parâmetro
-  const name = req.params.name;
-  // Enviando resposta para a rota
-  res.send('Olá ' + name + '!');
+  const name = req.query['name'];
+  if (name) res.send('Olá ' + name + '!');
+  else res.send('Olá!');
 });
 
 app.listen(5500, (error) => {
