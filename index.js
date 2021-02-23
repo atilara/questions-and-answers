e6846664647a021a51b6a264d8892de88529fb90
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-02-23 14:57:15 -0300

    Utilizando bodyParser para requisições do tipo POST

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import bodyParser from 'body-parser';
 // Realizando uma instância do express em uma constante,
 // para evitar sobrescrevê-la
 const app = express();
@@ -7,6 +7,12 @@ const app = express();
 // Definindo pro express que o EJS vai ser utilizado pra renderizar HTML
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+// Utilizado para pegar os dados enviados pelas requisições do tipo POST
+// Decodifica os dados
+app.use(bodyParser.urlencoded({ extended: false }));
+// Pegando dados enviados em formato de JSON
+app.use(bodyParser.json());
 
 // Rota do tipo GET e seu caminho, recebendo request e response como params
 app.get('/', (req, res) => {
@@ -16,6 +22,17 @@ app.get('/', (req, res) => {
 
 app.get('/ask', (req, res) => {
   res.render('ask');
+});
+
+// Rota POST para receber dados do formulário
+app.post('/save_question', (req, res) => {
+  // req.body.title pega os dados enviados pelo input com name="title"
+  // O body é disponibilizado pelo bodyParser
+  var title = req.body.title;
+  var description = req.body.description;
+  res.send(
+    'Formulário recebido! Título: ' + title + ' | Descrição: ' + description,
+  );
 });
 
 app.listen(5500, (error) => {
